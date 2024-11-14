9fac770406ba07f7ef6439d595cea223b9de4ca7
Kiefer <kiefx@pm.me>
2024-11-13 18:04:19 -0800

    Adding commit

--- 226/labs/lab7/website/battleship/models.py
+++ 226/labs/lab7/website/battleship/models.py
@@ -6,8 +6,14 @@ class Tile(models.Model):
     column = models.IntegerField()
     value = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.row} - {self.column} - {self.value}"
+
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
     score = models.IntegerField()
     is_turn = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name} - {self.score}"
