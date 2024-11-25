e8069a7042f1554c960a8a0509f15b0298c8554d
Kiefer <kiefx@pm.me>
2024-11-24 17:44:01 -0800

    NPM works?

--- 226/labs/lab9/build.sh
+++ 226/labs/lab9/build.sh
@@ -3,4 +3,7 @@ pipenv run
 pipenv install
 pipenv run pip install -r website/battleship/requirements.txt
 pipenv run python website/manage.py collectstatic --no-input
+pipenv run python website/manage.py makemigrations
 pipenv run python website/manage.py migrate
+npm install website/bs_theme/static_src
+pipenv run python website/manage.py tailwind build
