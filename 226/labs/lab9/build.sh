a20b9b2a37ea45e707edca033fe7658602458dbe
Kiefer <kiefx@pm.me>
2024-11-24 17:14:56 -0800

    Build

--- 226/labs/lab9/build.sh
+++ 226/labs/lab9/build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-pipenv install
-pipenv run python website/manage.py collectstatic --no-input
-pipenv run python website/manage.py migrate
+# Install Python dependencies
+pip install --upgrade pip
+pip install pipenv
+pipenv install --deploy --ignore-pipfile
+
+# Install Node.js dependencies for Tailwind CSS
+cd bs_theme/static_src
+npm install
+cd -
+
+# Build Tailwind CSS
+pipenv run python manage.py tailwind build
+
+# Collect static files
+pipenv run python manage.py collectstatic --no-input
+
+# Apply database migrations
+pipenv run python manage.py migrate
