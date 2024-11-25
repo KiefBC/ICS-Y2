6630b8a2053e36d2337fef1abb53caaf52b900fa
Kiefer <kiefx@pm.me>
2024-11-24 17:34:44 -0800

    Tailwind working now?

--- 226/labs/lab9/build.sh
+++ 226/labs/lab9/build.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-pipenv run 
-pipenv install
-pipenv run python website/manage.py collectstatic --no-input
-pipenv run python website/manage.py migrate
+
+echo -e "\nStarting build process...\n"
+
+# Install Python dependencies
+echo -e "\nInstalling Python dependencies...\n"
+python -m pip install --upgrade pip
+pip install pipenv
+pipenv install --system --deploy
+
+# Install Node.js and build Tailwind
+echo -e "\nInstalling Node.js dependencies and building Tailwind...\n"
+cd website/bs_theme/static_src
+npm install
+npm run build
+cd ../../..
+
+# Run Django commands
+echo -e "\nRunning Django commands...\n"
+cd website
+python manage.py collectstatic --no-input
+python manage.py migrate
+cd ..
+
+echo -e "\nBuild process complete!\n"
