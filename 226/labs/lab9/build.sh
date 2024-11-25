83f33207f0c82ed181c05fdd27d9fd86d299d1ff
Kiefer <kiefx@pm.me>
2024-11-24 17:03:29 -0800

    Test

--- 226/labs/lab9/build.sh
+++ 226/labs/lab9/build.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# Install dependencies
-python -m pip install --upgrade pip
-pip install pipenv
-pipenv install --system --deploy
-
-# Install Django and other requirements
-cd website/battleship
-pip install -r requirements.txt
-cd ../..
-
-# Run Django commands
-cd website
-python manage.py collectstatic --no-input
-python manage.py migrate
+pipenv run pipenv install
+pipenv run python website/manage.py collectstatic --no-input
+pipenv run python website/manage.py migrate
