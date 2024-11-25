62ca16b10c91cfab5249264e79f96451b466db94
Kiefer <kiefx@pm.me>
2024-11-24 17:51:56 -0800

    Maybe just run it instead?

--- 226/labs/lab9/build.sh
+++ 226/labs/lab9/build.sh
@@ -6,4 +6,5 @@ pipenv run python website/manage.py collectstatic --no-input
 pipenv run python website/manage.py makemigrations
 pipenv run python website/manage.py migrate
 npm install website/bs_theme/static_src
-pipenv run python website/manage.py tailwind build
+# pipenv run python website/manage.py tailwind build
+pipenv run python website/manage.py tailwind start
